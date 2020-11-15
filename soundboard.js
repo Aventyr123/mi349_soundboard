772081670ccbacce8078e70cf7ab9013acb765aa
Minshen Li <liminshe@msu.edu>
2020-11-15 10:13:33 -0500

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,11 @@
 document.getElementById('blip').onclick = function () {
     document.getElementById('audio1').play();
 }
+
+document.getElementById('bloop').onclick = function () {
+    document.getElementById('audio2').play();
+}
+
+document.getElementById('blurp').onclick = function () {
+    document.getElementById('audio3').play();
+}
