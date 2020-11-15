b5ee16a892b1a563fb9f67ef7121a97a95f5e5b5
Minshen Li <liminshe@msu.edu>
2020-11-15 09:31:39 -0500

    update

---
+++ soundboard.js
@@ -0,0 +1,11 @@
+function click1() {
+    document.sound.src = "blip.wav";
+}
+
+function click2() {
+    document.sound.src = "bloop_x.wav";
+}
+
+function click3() {
+    document.sound.src = "blurp_x.wav";
+}
