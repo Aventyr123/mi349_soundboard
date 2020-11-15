9ac0081845d23fc3c494794be10cedff077b392a
Minshen Li <liminshe@msu.edu>
2020-11-15 09:46:23 -0500

    update

--- soundboard.js
+++ soundboard.js
@@ -1,1 +1,5 @@
-document.getElementById('blurp').onclick = document.getElementById("blurp").play();
+var buttonElement = document.getElementById('blip')
+var blip = document.getElementById('audio1')
+buttonElement.addEventListener('click', function(){
+   blip.play()
+})
